ae6b729932b3637f6b16956e467a83c075a34433
Ewan-Brown <ewanbrown2000@gmail.com>
2016-06-13 21:04:02 -0400

    Fixed Textures yay

--- forge/src/main/java/com/mymods/virusmod/VirusMod.java
+++ forge/src/main/java/com/mymods/virusmod/VirusMod.java
@@ -21,7 +21,7 @@ import net.minecraftforge.common.config.Configuration;
 @Mod(modid = VirusMod.ID, version = VirusMod.VERSION, name = VirusMod.NAME, guiFactory = "com.mymods.virusmod.ModGuiFactory")
 public class VirusMod {
 	public static final String ID = "virusmod";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.1";
 	public static final String NAME = "VirusMod";
 
 
